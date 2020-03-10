bacd2f6023d038775b2abd2499ad6ab7c2c26621
efterr <56841172+efterr@users.noreply.github.com>
2020-03-09 20:59:44 -0700

    Add files via upload

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: eseidame <eseidame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 22:50:47 by esidame           #+#    #+#             */
-/*   Updated: 2020/03/09 20:12:01 by eseidame         ###   ########.fr       */
+/*   Updated: 2020/03/09 20:56:12 by eseidame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,17 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
-	{
-		n = -n;
-		ft_putchar('-');
-	}
-	if (n < 10 && n >= 0)
-		ft_putchar(n + '0');
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
 	else
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n *= -1;
+		}
+		if (n >= 10)
+			ft_putnbr_fd(n / 10, fd);
 	}
+	ft_putchar_fd((n % 10) + '0', fd);
 }
